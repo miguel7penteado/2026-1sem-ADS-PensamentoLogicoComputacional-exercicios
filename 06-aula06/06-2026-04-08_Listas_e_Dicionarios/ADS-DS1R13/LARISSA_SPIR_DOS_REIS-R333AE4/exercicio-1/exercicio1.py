02299fbb2f78fc999c11be4b9e2d7893be630a1b
Miguél Suares <miguel7penteado@gmail.com>
2026-04-08 21:57:25 -0300

    Add files via upload

--- 06-aula06/06-2026-04-08_Listas_e_Dicionarios/ADS-DS1R13/LARISSA_SPIR_DOS_REIS-R333AE4/exercicio-1/exercicio1.py
+++ 06-aula06/06-2026-04-08_Listas_e_Dicionarios/ADS-DS1R13/LARISSA_SPIR_DOS_REIS-R333AE4/exercicio-1/exercicio1.py
@@ -1,1 +1,7 @@
-#exercicio1
+
+geocodigos = ["3501", "3302", "3505", "4101"]
+sp_codes = [code for code in geocodigos if code.startswith("35")]
+# Resultado: ['3501', '3505']
+
+print(f"o valor da lista é {geocodigos}")
+print(f"geocodigos de são paulo: {sp_codes}")
---
+++ 06-aula06/06-2026-04-08_Listas_e_Dicionarios/ADS-DS1R13/LARISSA_SPIR_DOS_REIS-R333AE4/exercicio-1/exercicio2.py
@@ -0,0 +1,7 @@
+populacoes = [120, 450, 300, 210]
+media = sum(populacoes) / len(populacoes)
+
+print(f" populacao por setor censitario eh: {populacoes}")
+print(f" a media de pessoas por setor censitario eh: {media}")
+
+
